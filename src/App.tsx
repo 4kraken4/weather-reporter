278f6d909831aa3e58d6be996efe4e801c52e091
Kraken <warunamadushanka456@gmail.com>
2025-06-19 17:30:45 +0530

    feat: configure primereact for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import './App.css';
-
 import { RouterProvider } from '@core/router/RouterProvider';
+import 'primeflex/primeflex.css'; // flex
+import 'primeicons/primeicons.css'; // icons
+import 'primereact/resources/primereact.min.css'; // core css
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
